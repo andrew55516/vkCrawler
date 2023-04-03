ee3e98b50992f761da4ab01db18ac2de0d515b08
andrey <andrey.aksenov2001@gmail.com>
2023-04-03 03:01:26 +0300

    nodes and edges tables

---
+++ db/migration/000002_create_nodes_edges.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS "likes_edges";
+DROP TABLE IF EXISTS "comments_edges";
+DROP TABLE IF EXISTS "all_edges";
+DROP TABLE IF EXISTS "likes_nodes";
+DROP TABLE IF EXISTS "comments_nodes";
+DROP TABLE IF EXISTS "all_nodes";
---
+++ db/migration/000002_create_nodes_edges.up.sql
@@ -0,0 +1,60 @@
+CREATE TABLE IF NOT EXISTS "likes_nodes"
+(
+    "id"         bigserial PRIMARY KEY,
+    "label"      varchar     NOT NULL,
+    "start" timestamptz NOT NULL DEFAULT (now()),
+    "end"   timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE IF NOT EXISTS "comments_nodes"
+(
+    "id"         bigserial PRIMARY KEY,
+    "label"      varchar     NOT NULL,
+    "start" timestamptz NOT NULL DEFAULT (now()),
+    "end"   timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE IF NOT EXISTS "all_nodes"
+(
+    "id"         bigserial PRIMARY KEY,
+    "label"      varchar     NOT NULL,
+    "start" date NOT NULL DEFAULT (now()),
+    "end"   date NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE IF NOT EXISTS "likes_edges"
+(
+    "id"         bigserial PRIMARY KEY,
+    "source"      bigint     NOT NULL,
+    "target"      bigint     NOT NULL,
+--     "weight"      bigint     NOT NULL,
+    "start" timestamptz NOT NULL DEFAULT (now()),
+    "end"   timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE IF NOT EXISTS "comments_edges"
+(
+    "id"         bigserial PRIMARY KEY,
+    "source"      bigint     NOT NULL,
+    "target"      bigint     NOT NULL,
+--     "weight"      bigint     NOT NULL,
+    "start" timestamptz NOT NULL DEFAULT (now()),
+    "end"   timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE IF NOT EXISTS "all_edges"
+(
+    "id"         bigserial PRIMARY KEY,
+    "source"      bigint     NOT NULL,
+    "target"      bigint     NOT NULL,
+--     "weight"      bigint     NOT NULL,
+    "start" date NOT NULL DEFAULT (now()),
+    "end"   date NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "likes_edges" ADD FOREIGN KEY ("source") REFERENCES "likes_nodes" ("id");
+ALTER TABLE "likes_edges" ADD FOREIGN KEY ("target") REFERENCES "likes_nodes" ("id");
+ALTER TABLE "comments_edges" ADD FOREIGN KEY ("source") REFERENCES "comments_nodes" ("id");
+ALTER TABLE "comments_edges" ADD FOREIGN KEY ("target") REFERENCES "comments_nodes" ("id");
+ALTER TABLE "all_edges" ADD FOREIGN KEY ("source") REFERENCES "all_nodes" ("id");
+ALTER TABLE "all_edges" ADD FOREIGN KEY ("target") REFERENCES "all_nodes" ("id");
